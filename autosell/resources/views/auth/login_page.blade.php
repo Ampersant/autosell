86fb03ebca5c694277696d9fa64ccee6a9cb65b0
Ampersant <pika4yxa1337@gmail.com>
2024-04-01 23:34:51 +0200

    2

--- autosell/resources/views/auth/login_page.blade.php
+++ autosell/resources/views/auth/login_page.blade.php
@@ -1,3 +1,41 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="{{ asset('css/login-register.css')}}" rel="stylesheet" />
+	<script src="{{ asset('js/login-register.js')}}" type="text/javascript"></script>
+
+    <title>Login form</title>
+</head>
+<body>
+    <div class="content registerBox" >
+        <div class="container" style="width: 60vh;"><div class="form">
+                @if ($errors->any())
+                   <div class="alert alert-danger">
+                       <ul>
+                           @foreach ($errors->all() as $error)
+                               <li>{{ $error }}</li>
+                           @endforeach
+                       </ul>
+                   </div>
+             @endif
+           <form method="post" html="{:multipart=>true}" data-remote="true" action="{{ route('login') }}" accept-charset="UTF-8">
+           @csrf
+            <input id="email" class="form-control mt-3" type="text" placeholder="Email" name="email">
+           <input id="password" class="form-control mt-3" type="password" placeholder="Password" name="password"> 
+           <input class="btn btn-default btn-register mt-3" type="submit" value="Login" name="commit">
+           </form>
+           </div>
+       </div>
+    </div>
+
+</body>
+</html>
--- autosell/resources/views/profile/profile.blade.php
+++ autosell/resources/views/profile/profile.blade.php
@@ -1,3 +1,7 @@
 <div>
     <h1>ITS MY PROFILE!!!</h1>
+    <form action="{{ route('logout')}}" method="post">
+        @csrf
+        <input type="submit" value="Logout">
+    </form>
 </div>
